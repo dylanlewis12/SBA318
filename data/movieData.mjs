fd0c3e1148dce6155fc5766aa638a419bc429134
Dylan Lewis <hi4491@wayne.edu>
2026-01-27 17:01:00 -0500

    Create data directo ry and file to store movieCatalog

---
+++ data/movieData.mjs
@@ -0,0 +1,41 @@
+let movieData = [];
+
+/*
+export async function loadMovies() {
+    try {
+        // Generate a random number between 1 and 1025 (valid Pokémon range)
+        const API_URL = 'https://api.tvmaze.com/shows';
+        let response = await axios.get(API_URL);
+
+        let result = response.data;
+
+        movieData = result;
+        return movieData;
+
+    } catch(error) {
+        console.log('Error fetching movie collection:', error.message);
+        return [];
+    }
+}
+*/
+
+async function loadMovies() {
+    try {
+        // Generate a random number between 1 and 1025 (valid Pokémon range)
+        const API_URL = 'https://api.tvmaze.com/shows';
+        let response = await axios.get(API_URL);
+
+        let result = response.data;
+
+        movieData = result;
+
+        console.log(movieData);
+        return movieData;
+
+    } catch(error) {
+        console.log('Error fetching movie collection:', error.message);
+        return [];
+    }
+}
+
+loadMovies();
